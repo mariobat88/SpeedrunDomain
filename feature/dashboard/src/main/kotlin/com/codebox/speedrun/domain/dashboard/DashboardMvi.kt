37be74de5696b08ef60c77dc933ba257ee4a42a3
mariobat <mario.bat.business@gmail.com>
2022-10-11 13:50:30 +0200

    import fix

--- feature/dashboard/src/main/kotlin/com/codebox/speedrun/domain/dashboard/DashboardMvi.kt
+++ feature/dashboard/src/main/kotlin/com/codebox/speedrun/domain/dashboard/DashboardMvi.kt
@@ -1,8 +1,6 @@
 package com.codebox.speedrun.domain.dashboard
 
 import androidx.navigation.NavOptions
-import com.codebox.speedrun.domain.data.repo.games.model.GameModel
-import com.codebox.speedrun.domain.repo.runs.model.RunModel
 
 sealed class Intent {
     data class NavigateToTab(val route: String, val navOptions: NavOptions) : Intent()
