4e5a5ef1de81bab19b27a02611ccec2e4cd65bfe
mariobat <mario.bat.business@gmail.com>
2022-11-03 19:59:24 +0100

    dynamicColors support

--- app/src/main/kotlin/com/speedrun/domain/ui/AppScreen.kt
+++ app/src/main/kotlin/com/speedrun/domain/ui/AppScreen.kt
@@ -1,15 +1,30 @@
 package com.speedrun.domain.ui
 
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.rememberNavController
 import com.speedrun.domain.core.designsystem.theme.LightDefaultColorScheme
 import com.speedrun.domain.navigation.AppNavigation
 
 @Composable
 fun AppScreen() {
+    val darkTheme = isSystemInDarkTheme()
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
+    val colors = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> LightDefaultColorScheme
+        else -> LightDefaultColorScheme
+    }
+
     MaterialTheme(
-        colorScheme = LightDefaultColorScheme
+        colorScheme = colors,
     ) {
         val appViewModel = AppViewModel.create()
         val navController = rememberNavController()
